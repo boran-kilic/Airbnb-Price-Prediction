8881143d7d18539dc721a45af856e5118780e0f3
boran-kilic <165495405+boran-kilic@users.noreply.github.com>
2024-03-30 21:38:49 +0300

    bfcfb
     fc

--- firstcode.py
+++ firstcode.py
@@ -5,13 +5,10 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("Airbnb_Data.csv")
 print(data.head)
 data.head()
-<<<<<<< Updated upstream
 """
 d s 
 cd d d
  d ds sdcsdcas
  
  scc"""
-=======
 "aaaaaaaaa"
->>>>>>> Stashed changes
